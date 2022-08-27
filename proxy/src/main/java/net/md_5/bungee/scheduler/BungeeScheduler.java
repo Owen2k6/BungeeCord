78779ffc39df21555fbd2a065757ad092f128204
Strultz <69481716+Strultz@users.noreply.github.com>
2022-08-26 20:33:36 -0500

    It make sense now
    I TRY THIS NOW

--- proxy/src/main/java/net/md_5/bungee/scheduler/BungeeScheduler.java
+++ proxy/src/main/java/net/md_5/bungee/scheduler/BungeeScheduler.java
@@ -1,10 +1,20 @@
 package net.md_5.bungee.scheduler;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import gnu.trove.TCollections;
 import gnu.trove.iterator.TIntObjectIterator;
 import gnu.trove.map.TIntObjectMap;
 import gnu.trove.map.hash.TIntObjectHashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import net.md_5.bungee.BungeeCord;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.scheduler.ScheduledTask;
@@ -16,37 +26,43 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class BungeeScheduler implements TaskScheduler
 {
 
+    private final ExecutorService s = Executors.newCachedThreadPool( new ThreadFactoryBuilder().setNameFormat( "Bungee Pool Thread #%1$d" ).build() );
     private final AtomicInteger taskCounter = new AtomicInteger();
     private final TIntObjectMap<BungeeTask> tasks = TCollections.synchronizedMap( new TIntObjectHashMap<BungeeTask>() );
+    private final Multimap<Plugin, BungeeTask> tasksByPlugin = Multimaps.synchronizedMultimap( HashMultimap.<Plugin, BungeeTask>create() );
+
+    public void shutdown()
+    {
+        s.shutdown();
+    }
 
     @Override
     public void cancel(int id)
     {
-        cancel( tasks.remove( id ) );
+        BungeeTask task = tasks.remove( id );
+        task.cancel();
+        tasksByPlugin.values().remove( task );
     }
 
     @Override
     public void cancel(ScheduledTask task)
     {
-        Preconditions.checkArgument( task instanceof BungeeTask, "Don't know how to handle task %s", task );
-        tasks.remove( task.getId() ).getFuture().cancel( false );
+        cancel( task.getId() );
     }
 
     @Override
     public int cancel(Plugin plugin)
     {
-        int cancelled = 0;
-        for ( TIntObjectIterator<BungeeTask> iter = tasks.iterator(); iter.hasNext(); )
+        Set<ScheduledTask> toRemove = new HashSet<>();
+        for ( ScheduledTask task : tasksByPlugin.get( plugin ) )
         {
-            BungeeTask task = iter.value();
-            if ( task.getOwner() == plugin )
-            {
-                task.getFuture().cancel( false );
-                iter.remove();
-                cancelled++;
-            }
+            toRemove.add( task );
         }
-        return cancelled;
+        for ( ScheduledTask task : toRemove )
+        {
+            cancel( task );
+        }
+        return toRemove.size();
     }
 
     @Override
@@ -58,21 +74,17 @@ public class BungeeScheduler implements TaskScheduler
     @Override
     public ScheduledTask schedule(Plugin owner, Runnable task, long delay, TimeUnit unit)
     {
-        return prepare( owner, task ).setFuture( BungeeCord.getInstance().executors.schedule( task, delay, unit ) );
+        return schedule( owner, task, delay, 0, unit );
     }
 
     @Override
     public ScheduledTask schedule(Plugin owner, Runnable task, long delay, long period, TimeUnit unit)
     {
-        return prepare( owner, task ).setFuture( BungeeCord.getInstance().executors.scheduleWithFixedDelay( task, delay, period, unit ) );
-    }
-
-    private BungeeTask prepare(Plugin owner, Runnable task)
-    {
         Preconditions.checkNotNull( owner, "owner" );
         Preconditions.checkNotNull( task, "task" );
-        BungeeTask prepared = new BungeeTask( taskCounter.getAndIncrement(), owner, task );
+        BungeeTask prepared = new BungeeTask( this, taskCounter.getAndIncrement(), owner, task, delay, period, unit );
         tasks.put( prepared.getId(), prepared );
+        s.execute( prepared );
         return prepared;
     }
 }
